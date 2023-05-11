eb4f6ab5b846fd5b7fc9df47f59a37097dd85ff3
HoMaiQue <homaique.it@gmail.com>
2023-05-11 22:51:01 +0700

    add handle rent modal

--- app/components/Modal/RentModal.tsx
+++ app/components/Modal/RentModal.tsx
@@ -1,13 +1,21 @@
 "use client";
-import React, { useMemo, useState } from "react";
-import Modal from "./Modal";
 import { useRentModal } from "@/app/hooks/useRentModal";
-import { categories } from "../Navbar/Categories";
+import { schema } from "@/app/utils/rules";
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useMemo, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 import Heading from "../Heading";
+import Input from "../Input";
 import CategoryInput from "../Input/CategoryInput";
-import { FieldValues, useForm } from "react-hook-form";
+import Counter from "../Input/Counter";
 import CountrySelect from "../Input/CountrySelect";
-import Map from "../Map";
+import ImageUpload from "../Input/ImageUpload";
+import { categories } from "../Navbar/Categories";
+import Modal from "./Modal";
 enum STEPS {
     CATEGORY = 0,
     LOCATION = 1,
@@ -16,31 +24,56 @@ enum STEPS {
     DESCRIPTION = 4,
     PRICE = 5,
 }
+type FormData = {
+    category: string;
+    location: any;
+    guestCount: number;
+    roomCount: number;
+    bathroomCount: number;
+    imageSrc: string;
+    price: number;
+    title: string;
+    description: string;
+};
+const rentSchema = schema.pick(["title", "description"]);
 export default function RentModal() {
     const rentModal = useRentModal();
     const [step, setStep] = useState(STEPS.CATEGORY);
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
     const {
         watch,
         register,
         formState: { errors },
         handleSubmit,
         setValue,
-    } = useForm<FieldValues>({
+        reset,
+    } = useForm<FormData>({
         defaultValues: {
             category: "",
             location: null,
             guestCount: 1,
             roomCount: 1,
+            bathroomCount: 1,
             imageSrc: "",
             price: 1,
             title: "",
             description: "",
         },
+        resolver: yupResolver(rentSchema),
     });
 
     const category = watch("category");
     const location = watch("location");
-    const setCustomValue = (name: string, value: any) => {
+    const guestCount = watch("guestCount");
+    const roomCount = watch("roomCount");
+    const bathroomCount = watch("bathroomCount");
+    const imageSrc = watch("imageSrc");
+    const Map = useMemo(
+        () => dynamic(() => import("../Map"), { ssr: false }),
+        [location]
+    );
+    const setCustomValue = (name: keyof FormData ,value: any) => {
         setValue(name, value, {
             shouldDirty: true,
             shouldTouch: true,
@@ -52,6 +85,30 @@ export default function RentModal() {
     };
     const handleNext = () => {
         setStep((value) => value + 1);
+    };
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        if (step !== STEPS.PRICE) {
+           
+            return handleNext();
+        }
+       
+        setIsLoading(true);
+
+        axios
+            .post("/api/listings", data)
+            .then(() => {
+                toast.success("Listing created");
+                router.refresh();
+                reset();
+                setStep(STEPS.CATEGORY);
+                rentModal.onClose();
+            })
+            .catch((e) => {
+                toast.error("Something went wrong");
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
     };
     const actionLabel = useMemo(() => {
         if (step === STEPS.PRICE) {
@@ -109,7 +166,98 @@ export default function RentModal() {
                     value={location}
                     onChange={(value) => setCustomValue("location", value)}
                 />
-                <Map />
+                <Map center={location?.latlng} />
+            </div>
+        );
+    }
+
+    if (STEPS.INFO === step) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Share some basics about your place"
+                    subtitle="What amenities do you have"
+                />
+                <Counter
+                    title="Guest"
+                    subTitle="How many guest do you  allow ?"
+                    value={guestCount}
+                    onChange={(value) => setCustomValue("guestCount", value)}
+                />
+                <hr />
+                <Counter
+                    title="Rooms"
+                    subTitle="How many room do you have  ?"
+                    value={roomCount}
+                    onChange={(value) => setCustomValue("roomCount", value)}
+                />
+                <hr />
+                <Counter
+                    title="Bathrooms"
+                    subTitle="How many bathrooms do you have ?"
+                    value={bathroomCount}
+                    onChange={(value) => setCustomValue("bathroomCount", value)}
+                />
+            </div>
+        );
+    }
+    if (STEPS.IMAGES === step) {
+        bodyContent = (
+            <div className="flex flex-col ">
+                <Heading
+                    title="Add a photo of your place"
+                    subtitle="Show guest what your place looks like"
+                />
+                <ImageUpload
+                    value={imageSrc}
+                    onChange={(value) => setCustomValue("imageSrc", value)}
+                />
+            </div>
+        );
+    }
+    if (STEPS.DESCRIPTION === step) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="How would you describe your place"
+                    subtitle="Short and sweets works best"
+                />
+                <Input
+                    name="title"
+                    label="Title"
+                    disabled={isLoading}
+                    register={register}
+                    errorMessage={errors.title?.message}
+                    
+                />
+                <hr />
+                <Input
+                    name="description"
+                    label="Description"
+                    disabled={isLoading}
+                    register={register}
+                    errorMessage={errors.description?.message}
+                    
+                />
+            </div>
+        );
+    }
+    if (STEPS.PRICE === step) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Now, set your pice"
+                    subtitle="How much do you charge per night"
+                />
+                <Input
+                    name="price"
+                    disabled={isLoading}
+                    register={register}
+                    label="pice"
+                    formatPrice
+                    type="number"
+                    
+                />
             </div>
         );
     }
@@ -118,7 +266,7 @@ export default function RentModal() {
             isOpen={rentModal.isOpen}
             title="Airbnb your home"
             onClose={rentModal.onClose}
-            onSubmit={handleNext}
+            onSubmit={handleSubmit(onSubmit)}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
             secondaryAction={step === STEPS.CATEGORY ? undefined : handleBack}
--- app/utils/rules.ts
+++ app/utils/rules.ts
@@ -3,6 +3,8 @@ export const schema = yup.object({
     email: yup.string().required(""),
     name: yup.string().required(""),
     password: yup.string().required(""),
+    title: yup.string(),
+    description: yup.string(),
     
 })
 export type Schema = yup.InferType<typeof schema>
